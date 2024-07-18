9ed913b4e81b8fb22ec168d00b653cd6b105947c
juntakamine <joystep1224@gmail.com>
2024-07-18 15:12:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(page_title="my Great ChatGPT,Page_icon="★"
 )
