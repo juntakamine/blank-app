ae93f36fbea8b478319cbb5907ebf30a86534876
juntakamine <joystep1224@gmail.com>
2024-07-18 15:29:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 form langchain.chat_models import ChatOpenAi
 form langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+def main():
+     llm = ChatOpenAI(temperature=0)
 
 st.set_page_config(page_title="my Great ChatGPT,
 Page_icon="★"
